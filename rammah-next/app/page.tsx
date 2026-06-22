2d717c102d4f5eb022ea1462ceca344e7db0d86b
extacademyedu-lang <Muhammedmekky4@gmail.com>
2026-06-22 21:15:23 +0300

    feat: extract footer and add location text

--- rammah-next/app/page.tsx
+++ rammah-next/app/page.tsx
@@ -1,31 +1,6 @@
-"use client";
-
-import { useState } from "react";
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import StatementSection from "@/components/StatementSection";
-import MethodologySection from "@/components/MethodologySection";
-import AboutSection from "@/components/AboutSection";
-import CTASection from "@/components/CTASection";
-import ServicesSection from "@/components/ServicesSection";
-import LoadingScreen from "@/components/LoadingScreen";
+import HomeClient from "@/components/HomeClient";
+import Footer from "@/components/Footer";
 
 export default function Home() {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  return (
-    <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-
-      <main className="bg-black min-h-[100dvh] overflow-x-clip">
-        <Navbar entryReady={isLoaded} />
-        <HeroSection entryReady={isLoaded} />
-        <StatementSection />
-        <MethodologySection />
-        <AboutSection />
-        <ServicesSection />
-        <CTASection />
-      </main>
-    </>
-  );
+  return <HomeClient footer={<Footer />} />;
 }
