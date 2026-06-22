5b40a7e189e8f00fa83b1f835d452ed3e1e6abc4
extacademyedu-lang <Muhammedmekky4@gmail.com>
2026-06-22 21:15:23 +0300

    feat: extract footer and add location text

---
+++ components/HomeClient.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import StatementSection from "@/components/StatementSection";
+import MethodologySection from "@/components/MethodologySection";
+import AboutSection from "@/components/AboutSection";
+import CTASection from "@/components/CTASection";
+import ServicesSection from "@/components/ServicesSection";
+import LoadingScreen from "@/components/LoadingScreen";
+
+export default function HomeClient({ footer }: { footer: React.ReactNode }) {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+
+      <main className="bg-black min-h-[100dvh] overflow-x-clip">
+        <Navbar entryReady={isLoaded} />
+        <HeroSection entryReady={isLoaded} />
+        <StatementSection />
+        <MethodologySection />
+        <AboutSection />
+        <ServicesSection />
+        <CTASection />
+        {footer}
+      </main>
+    </>
+  );
+}
